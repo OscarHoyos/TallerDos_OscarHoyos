1b4cef9344c1cf6f96ca7853e9b15a9b0f6a1c75
oscar Hoyos <oscarht597@hotmail.com>
2018-05-12 23:54:47 -0500

    prueba2

--- Index.js
+++ Index.js
@@ -13,12 +13,40 @@ const express = require ("express"),
 
     app.use(express.static('public'));
 
+    MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+        if (err) throw err;
+    
+        db = client.db('local');
+    
+        app.listen(3000);
+    });
     
  
 app.get("/",(req, res) =>{
 
     
-    res.send("algo");
+    var RazerProductos = db.collection('RazerProductos').find();
+
+    RazerProductos.toArray((err, result) => {
+       // console.log(RazerProductos);
+        res.render("Index", {
+            RazerProductos : result
+        })
+    });
 });
-app.listen(3000);
+
+app.get("/RazerProductos/:Nombre", (req, res) => {
+    console.log("hola");
+    db.collection('RazerProductos').find (
+        {
+            Nombre: req.params.Nombre
+        }
+    ).toArray((err, result) => {
+        console.log(result[0]);
+        res.render('Index', {
+            local: result[0]
+        });
+    });
+});
+
 
