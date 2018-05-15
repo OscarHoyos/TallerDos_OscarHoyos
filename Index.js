6a59268589c04fb4756e5b084829483e743ac520
oscar Hoyos <oscarht597@hotmail.com>
2018-05-15 15:10:05 -0500

    imagenes y filtros

--- Index.js
+++ Index.js
@@ -27,8 +27,28 @@ app.get("/",(req, res) =>{
     
     var razerproductos = db.collection('razerproductos').find();
 
+    if (req.query.precio){
+        console.log(req.query.precio);
+
+        razerproductos.filter({precio : {
+            $gte: parseFloat(req.query.precio[0]),
+            $lte: parseFloat(req.query.precio[1])
+        }
+         });
+    }
+
+    if (req.query.tipo) 
+    razerproductos.filter({
+             tipo: req.query.tipo
+         });
+
+         if (req.query.color) 
+         razerproductos.filter({color : req.query.color});
+
     razerproductos.toArray((err, result) => {
-       console.log(razerproductos);
+
+
+       console.log("hi server");
         res.render("index", {
             razerproductos : result
         })
